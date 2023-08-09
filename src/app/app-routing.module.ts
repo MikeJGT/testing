fadf26dfec08d073c10bff7a83fa7d24bf55e733
miky3000 <mikecode808@gmail.com>
2023-08-09 18:03:15 +0200

    updating project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsListComponent } from './components/news/news-list/news-list.component';
+import { NewsFiltersComponent } from './components/news/news-filters/news-filters.component';
+import { SearchNewsComponent } from './components/pages/search-news/search-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: SearchNewsComponent },
+  { path: 'form', component: NewsFiltersComponent },
+  { path: '**', redirectTo: '' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewCardComponent } from './components/news/new-card/new-card.component';
+import { NewsListComponent } from './components/news/news-list/news-list.component';
+import { NewsFiltersComponent } from './components/news/news-filters/news-filters.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SearchNewsComponent } from './components/pages/search-news/search-news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCardComponent,
+    NewsListComponent,
+    NewsFiltersComponent,
+    SearchNewsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
